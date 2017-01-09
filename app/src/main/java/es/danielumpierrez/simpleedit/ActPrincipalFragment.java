2b70b12ad4d21c622860d89ca631f2e9e6fb237c
Daniel J. Umpiérrez <contacto@danielumpierrez.es>
2017-01-09 21:28:59 +0100

    Inicial commit

---
+++ app/src/main/java/es/danielumpierrez/simpleedit/ActPrincipalFragment.java
@@ -0,0 +1,22 @@
+package es.danielumpierrez.simpleedit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ActPrincipalFragment extends Fragment {
+
+    public ActPrincipalFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_act_principal, container, false);
+    }
+}
